a3c0eda1f9eca9aefd1768de6a2a6242ea29d676
Fabian Quintero <grovveip@gmail.com>
2024-07-05 11:23:25 -0500

    ok recursos en local

--- resources/views/maestra.blade.php
+++ resources/views/maestra.blade.php
@@ -15,22 +15,17 @@
     <script src="{{url("/js/jquery-3.7.0.js")}}"></script>
     <script src="{{url("/js/jquery.dataTables.min.js")}}"></script>
     <script src="{{url("/select_mio/bootstrap-select.min.js")}}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-   
-    <!-- DataTables Buttons CSS -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css" />
 
-    <!-- pdfMake Library -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/vfs_fonts.js"></script>
-
-    <!-- DataTables Buttons JS -->
-    <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
-    
+    <script src="{{url("/cdn/popper.min.js")}}"></script>
+    <script src="{{url("/cdn/bootstrap.min.js")}}"></script>
+    <link href="{{url("/cdn/buttons.dataTables.min.css")}}" rel="stylesheet">
+    <script src="{{url("/cdn/pdfmake.min.js")}}"></script>
+    <script src="{{url("/cdn/vfs_fonts.js")}}"></script>
+    <script src="{{url("/cdn/dataTables.buttons.min.js")}}"></script>
+    <script src="{{url("/cdn/jszip.min.js")}}"></script>
+    <script src="{{url("/cdn/buttons.html5.min.js")}}"></script>
     <script src="{{url("/select_search/select2.min.js")}}"></script>
+    
     <style>
         body {
             padding-top: 57px;
@@ -207,7 +202,7 @@
         .btn_modal {
             width: 25% !important;
             height: 70px;
-            font-size: 24px !important;
+            font-size: 20px !important;
         }
 
 
